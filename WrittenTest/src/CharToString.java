d6e9c7feb69750dca90bd9b90982f89388a7ffe0
chaitali kulkarni <ckulkarni289@gmail.com>
2018-05-13 17:35:54 +0200

    Example of Convert String to Char

---
+++ WrittenTest/src/CharToString.java
@@ -0,0 +1,15 @@
+
+public class CharToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "Germany";
+		char[] ch = s1.toCharArray();
+		for (int i = s1.length()-1; i >=0; i--) {
+			System.out.println(ch[i]);
+		}
+
+	}
+
+}
