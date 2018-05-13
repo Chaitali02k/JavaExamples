50c32be4d8e2357089d83d8cbafaffdcd6b248db
chaitali kulkarni <ckulkarni289@gmail.com>
2018-05-13 16:45:06 +0200

    example of String to UpperCase

---
+++ WrittenTest/src/UpperCaseString.java
@@ -0,0 +1,13 @@
+
+public class UpperCaseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="hello germany";
+		
+		System.out.println(s1.toUpperCase());
+		
+
+	}
+
+}
