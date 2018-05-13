db1057be1a1e9651e91e0d3c851e89be05e8d416
chaitali kulkarni <ckulkarni289@gmail.com>
2018-05-13 16:49:19 +0200

    Example of SubString

---
+++ WrittenTest/src/SubStringExample.java
@@ -0,0 +1,11 @@
+
+public class SubStringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="JavaPractice";
+		System.out.println("Substring is:"+s1.substring(5, 11));
+
+	}
+
+}
