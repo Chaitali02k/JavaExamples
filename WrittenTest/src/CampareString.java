6280d493e2aa45a037062ce34d8e922007711c97
chaitali kulkarni <ckulkarni289@gmail.com>
2018-05-13 17:36:14 +0200

    Exaple of camparison bwtween Strign

---
+++ WrittenTest/src/CampareString.java
@@ -0,0 +1,14 @@
+
+public class CampareString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1="Hallo";
+		String s2="Tschüs";
+		
+		System.out.println(s1.compareTo(s2));
+
+	}
+
+}
